9f3baac9abec1c5dd013b2532c5d2f53e1e4dbe3
Raí Vitor <raivitor@gmail.com>
2017-03-01 02:16:03 -0300

    Add persistencia

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -3,4 +3,24 @@ module.exports = function(app) {
 		console.log('recebida a requisição');
 		res.send('Ok Raí');
 	});
+
+	app.post('/pagamentos/pagamento', function(req, res) {
+		var pagamento = req.body;
+		console.log(pagamento);
+		pagamento.status = 'CRIADO';
+		pagamento.data = new Date();
+
+		var connection = app.persistencia.connectionFactory();
+		var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+		/*
+		pagamentoDao.lista(function (erro, resultado) {
+			if(erro) console.log(erro);
+			res.json(resultado);
+		});*/
+		
+		pagamentoDao.salva(pagamento, function (erro, resultado) {
+			console.log('salva');
+			res.json(pagamento);
+		});
+	});
 }
