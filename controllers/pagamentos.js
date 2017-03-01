9214e61ae79ff8990fa00c1118effa224d01d63f
Raí Vitor <raivitor@gmail.com>
2017-03-01 02:48:16 -0300

    Validações

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -6,21 +6,30 @@ module.exports = function(app) {
 
 	app.post('/pagamentos/pagamento', function(req, res) {
 		var pagamento = req.body;
-		console.log(pagamento);
 		pagamento.status = 'CRIADO';
 		pagamento.data = new Date();
 
+		req.assert("forma_de_pagamento", "Forma de pagamento é obrigatória.").notEmpty();
+		req.assert("valor", "Valor é obrigatório e deve ser um decimal.").notEmpty().isFloat();
+		req.assert("moeda", "Moeda é obrigatória e deve ter 3 caracteres").notEmpty().len(3, 3);
+
+		var errors = req.validationErrors();
+		if (errors) {
+			console.log("Erros de validação encontrados");
+			res.status(400).send(errors);
+			return;
+		}
 		var connection = app.persistencia.connectionFactory();
 		var pagamentoDao = new app.persistencia.PagamentoDao(connection);
-		/*
-		pagamentoDao.lista(function (erro, resultado) {
-			if(erro) console.log(erro);
-			res.json(resultado);
-		});*/
-		
-		pagamentoDao.salva(pagamento, function (erro, resultado) {
-			console.log('salva');
-			res.json(pagamento);
+		pagamentoDao.salva(pagamento, function(erro, resultado) {
+			if(erro) {
+				res.status(500).send(errors);
+			}
+			else{
+				console.log('Pagamento criado');
+				//res.location('/pagamentos/pagamento/' + resultado.insertId);
+				res.status(201).json(resultado);
+			}
 		});
 	});
 }
